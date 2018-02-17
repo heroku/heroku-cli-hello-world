d0bbd61ee8d1626bde252de422054df5964b5ea7
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-16 18:34:55 -0800

    fix: fixed tests

--- test/commands/hello/world.test.ts
+++ test/commands/hello/world.test.ts
@@ -1,17 +1,17 @@
 import {expect, test} from '@oclif/test'
 
-describe('hello', () => {
+describe('hello:world', () => {
   test
   .stdout()
-  .command(['hello'])
-  .it('runs hello', ctx => {
+  .command(['hello:world'])
+  .it('runs hello:world', ctx => {
     expect(ctx.stdout).to.contain('hello world from ./src/hello.ts!')
   })
 
   test
   .stdout()
-  .command(['hello', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
+  .command(['hello:world', '--name', 'jeff'])
+  .it('runs hello:world --name jeff', ctx => {
     expect(ctx.stdout).to.contain('hello jeff from ./src/hello.ts!')
   })
 })
